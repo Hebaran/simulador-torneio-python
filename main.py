ab5cc17643c0a06694f2f02eaabbcf0d911f9305
Heitor Rangel <heitor.roranm@gmail.com>
2025-07-29 20:13:43 -0300

    feat: Faz com que o mago tente utilizar o especial se disponível, caso contrário usa um ataque básico

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from models import Personagem, Guerreiro, Mago
-from utils import limpar_terminal
+from utils import limpar_terminal, subir_linha
 from random import shuffle, sample
 from time import sleep
 
@@ -30,6 +30,9 @@ while len(lutadores) > 1:
             ganhador = atacante if atacante.status_vida() else defensor
             perdedor = defensor if atacante.status_vida() else atacante
             
+            if hasattr(ganhador, "mana"):
+                ganhador.restaurar_mana()
+            
             ganhador.restaurar_hp()
             lutadores.remove(perdedor)
             break
@@ -41,22 +44,30 @@ while len(lutadores) > 1:
             atacante = duelistas[1]
             defensor = duelistas[0]
 
-        if atacante.mana >= 0:
-            atacante.mana = atacante.mana + 10 if atacante.mana <= atacante.mana_maxima - 10 else atacante.mana_maxima
-        relatorio_atacante = atacante.atacar(defensor)
+        try:
+            atacante.mana = min(atacante.mana + 10, atacante.mana_maxima)
+            relatorio_atacante = atacante.usar_especial(defensor)
+            
+            if not relatorio_atacante["especial"]:
+                raise PermissionError("Mana insuficiente para utilizar o especial.")
+        except (AttributeError, PermissionError):
+            relatorio_atacante = atacante.atacar(defensor)
+            
         
+        # NARRAÇÃO DO COMBATE
         limpar_terminal()
         print(f"======= {duelistas[0].nome} x {duelistas[1].nome} =======")
         print(f"Turno: {turno}/100\n")
         print(f"{relatorio_atacante["atacante"]} ataca {relatorio_atacante["alvo"]}.")
-        if relatorio_atacante.get("critico"):
-            print("CRÍTICO! ", end="")  
+        print(f"{atacante.nome} UTILIZA SEU ESPECIAL!" if relatorio_atacante.get("especial") else subir_linha())
+        print("GOLPE CRÍTICO! " if relatorio_atacante.get("critico") else "", end="")
         print(f"{relatorio_atacante["alvo"]} recebe {relatorio_atacante["dano_causado"]} de dano.")
         print(f"Vida restante de {relatorio_atacante["alvo"]}: {relatorio_atacante["vida_restante_alvo"]}")
+        print(f"Mana restante de {atacante.nome}: {atacante.mana}" if hasattr(atacante, "mana") else subir_linha())
         
-        sleep(0.25)
+        input()
 
     sleep(1)
 
 limpar_terminal()
-print(f"Parabéns, {lutadores[0].nome} é o ganhador do torneio!")
+print(f"Parabéns, {ganhador.nome} é o ganhador do torneio!")
